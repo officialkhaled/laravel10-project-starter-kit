5775d9edc23de216f7ba1a1d619971a146549f28
Khaled Hossain <khaledhossain778@gmail.com>
2025-02-19 22:23:01 +0600

    files add

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <p>{{ Carbon\Carbon::greetings() }}<span class="font-bold">{{ userName() }}</span>!</p>
                 </div>
             </div>
         </div>
